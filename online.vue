83e6d2e3325c4ce039edd49401cb572d7d570621
Adnan-Bacic <adnan-bacic@hotmail.com>
2021-01-20 19:31:30 +0100

    Initial commit

---
+++ online.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="online-status">
+        <div v-if="online" class="online">
+            <span class="light"></span>
+            <p>A staff member is online</p>
+        </div>
+        <div v-if="!online" class="offline">
+            <span class="light"></span>
+            <p>A staff member is not online</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+    props: ['status'],
+
+    data(){
+        return{
+            //online: true,
+        }
+    },
+
+    computed: {
+        online(){
+            return this.status == 'online';
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
